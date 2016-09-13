f8d8a9fba10304a8ebb4c742f74437ba878003f7
Björn Ritzl <bjorn.ritzl@king.com>
2016-09-13 11:04:00 +0200

    Push changes using travis

--- .travis/run.sh
+++ .travis/run.sh
@@ -1,3 +1,3 @@
 python check_size.py
-git commit -m "Generated new size report and graph" size.png report.csv
-git push origin master
+#git commit -m "Generated new size report and graph" size.png report.csv
+#git push origin master
