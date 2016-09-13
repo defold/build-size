a6b04e6473c3309a7fbda0bbe65383675ee43ffd
Björn Ritzl <bjorn.ritzl@king.com>
2016-09-13 21:19:45 +0200

    Added [skip ci] directive

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,5 @@
 git config --global user.email "nobody@nobody.org"
 git config --global user.name "Travis CI"
 
-git commit -m "Generated new size report and graph" size.png report.csv
+git commit -m "Generated new size report and graph [skip ci]" size.png report.csv
 git push "https://${GITHUB_TOKEN}@github.com/britzl/dmengine_size.git" HEAD:master
