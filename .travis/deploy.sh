e145840c7e86474a4e177c25823231c643e5d34c
Björn Ritzl <bjorn.ritzl@gmail.com>
2022-07-17 19:07:25 +0200

    Removed travis-ci

--- .travis/deploy.sh
+++
@@ -1,5 +0,0 @@
-git config --global user.email "bjorn.ritzl@gmail.com"
-git config --global user.name "Travis CI (bjorn.ritzl@gmail.com)"
-
-git commit -m "Generated new size report and graph [skip ci]" bundle_size.png engine_size.png engine_report.csv bundle_report.csv releases.json
-git push "https://${GITHUB_TOKEN}@github.com/britzl/dmengine_size.git" HEAD:master
